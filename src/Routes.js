ad646a69aa20df763d0eefb839ab14a096f5264b
themak01 <37306115+themak01@users.noreply.github.com>
2020-10-27 11:25:14 +0530

    Add files via upload

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+import Post from "./user/Post";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
+        <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+        <PrivateRoute path="/post/create" exact component={Post}/>
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
